7c59b43e6494a23b70ce2716f5d1a2178aae3335
Pallab Bag <bagmaster7@gmail.com>
2020-12-04 21:54:16 +0530

    php file uploaded

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD']=='POST'){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$message=$_POST['message'];
+
+		echo '<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Hi! $name </h4>
+  <p>Thank you for contact.</p>
+  <hr>
+  <p class="mb-0">We will reach you soon at $email.</p>
+</div>'
+	}
+
+
+?>
